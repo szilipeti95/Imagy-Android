76b3dd5ddfdc079b5c6059f0fe72c6be8b7da1a3
Szili Péter <peter.szili@kh.hu>
2020-05-09 13:21:11 +0200

    Add Datasources to. interactors

--- app/src/main/java/ml/mylittlebackend/jji6wk/domain/interactor/AuthInteractor.kt
+++ app/src/main/java/ml/mylittlebackend/jji6wk/domain/interactor/AuthInteractor.kt
@@ -1,8 +1,13 @@
 package ml.mylittlebackend.jji6wk.domain.interactor
 
+import ml.mylittlebackend.jji6wk.data.db.DiskDataSource
+import ml.mylittlebackend.jji6wk.data.network.NetworkDataSource
 import javax.inject.Inject
 
-class AuthInteractor @Inject constructor() {
+class AuthInteractor @Inject constructor(
+    diskDataSource: DiskDataSource,
+    networkDataSource: NetworkDataSource
+) {
     fun login() {
 
     }
--- app/src/main/java/ml/mylittlebackend/jji6wk/domain/interactor/UserInteractor.kt
+++ app/src/main/java/ml/mylittlebackend/jji6wk/domain/interactor/UserInteractor.kt
@@ -1,9 +1,14 @@
 package ml.mylittlebackend.jji6wk.domain.interactor
 
+import ml.mylittlebackend.jji6wk.data.db.DiskDataSource
+import ml.mylittlebackend.jji6wk.data.network.NetworkDataSource
 import ml.mylittlebackend.jji6wk.domain.model.User
 import javax.inject.Inject
 
-class UserInteractor @Inject constructor() {
+class UserInteractor @Inject constructor(
+    diskDataSource: DiskDataSource,
+    networkDataSource: NetworkDataSource
+) {
     fun getUser(): User {
         return User(email = "Dummy Email",
                     username = "Dummy Username",
